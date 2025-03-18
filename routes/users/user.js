b06e5a70b9abf5f202820c5c04fb383662cd8696
gagababutsidze <babutsidzegaga@gmail.com>
2025-03-18 04:22:49 -0700

    Fixed bcrypt issue

--- routes/users/user.js
+++ routes/users/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { queryDatabase } from "../../DBconnection.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv"
 dotenv.config()
