3a59d2833b3936895eb91b23afc71dc515dc2c9e
gagababutsidze <babutsidzegaga@gmail.com>
2025-04-05 23:21:50 -0700

    backend

--- authMiddleware.js
+++ authMiddleware.js
@@ -8,7 +8,7 @@ const authMiddleware = (req, res, next) => {
     if (!token) return res.status(401).json({ message: "No token provided" });
 
     try {
-        const verified = jwt.verify(token, "SECRET");
+        const verified = jwt.verify(token, process.env.SECRET);
         req.user = verified;
         next();
     } catch (error) {
