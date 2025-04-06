de9320b48a834675c79bb89e10c967fea40e4f1a
gagababutsidze <babutsidzegaga@gmail.com>
2025-04-06 06:55:13 -0700

    backend

--- error.js
+++ error.js
@@ -1,7 +1,7 @@
 export default (error, req, res, next) => {
  
    if (error.name ===  'JsonWebTokenError') {
-        return res.status(401).json({ error:error.message })
+        return res.status(401).json({ error: 'token is misssing!' })
       }
     
     else{
